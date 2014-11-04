417b3419ed3e61f6daccfbf7c21e2c5fdc714a9d
Ramon Navarro Bosch <ramon.nb@gmail.com>
2014-11-04 19:19:41 +0100

    Ara?

--- src/ctm.tunning/ctm/tunning/__init__.py
+++ src/ctm.tunning/ctm/tunning/__init__.py
@@ -125,3 +125,119 @@ def update_on_modify(obj, event):
 from collective.hootsuite import subscriber
 
 subscriber.update_on_modify = update_on_modify
+
+from Acquisition import aq_inner, aq_parent
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.utils import getToolByName
+from datetime import datetime
+from plone.app.layout.navigation.root import getNavigationRootObject
+from zope.component import providedBy
+from zope.component import queryUtility
+from zope.component.hooks import getSite
+from zope.i18n import translate
+from zope.i18nmessageid import MessageFactory
+from zope.schema.interfaces import IVocabularyFactory
+from z3c.form.interfaces import IAddForm
+from Products.CMFCore.interfaces._content import IFolderish
+from plone.uuid.interfaces import IUUID
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from zope.component import getMultiAdapter
+import json
+
+
+def get_portal():
+    closest_site = getSite()
+    if closest_site is not None:
+        for potential_portal in closest_site.aq_chain:
+            if ISiteRoot in providedBy(potential_portal):
+                return potential_portal
+
+
+def get_portal_url(context):
+    portal = get_portal()
+    if portal:
+        root = getNavigationRootObject(context, portal)
+        if root:
+            try:
+                return root.absolute_url()
+            except AttributeError:
+                return portal.absolute_url()
+        else:
+            return portal.absolute_url()
+    return ''
+
+
+def get_context_url(context):
+    if IAddForm.providedBy(context):
+        # Use the request URL if we are looking at an addform
+        url = context.request.get('URL')
+    elif hasattr(context, 'absolute_url'):
+        url = context.absolute_url
+        if callable(url):
+            url = url()
+    else:
+        url = get_portal_url(context)
+    return url
+
+def get_tinymce_options(context, field, request):
+    args = {'pattern_options': {}}
+    folder = context
+    if not IFolderish.providedBy(context):
+        folder = aq_parent(context)
+    if IPloneSiteRoot.providedBy(folder):
+        initial = None
+    else:
+        initial = IUUID(folder, None)
+    portal_url = get_portal_url(context)
+    current_path = folder.absolute_url()[len(portal_url):]
+
+    utility = getToolByName(aq_inner(context), 'portal_tinymce', None)
+    if utility:
+        # Plone 4.3
+        config = utility.getConfiguration(context=context,
+                                          field=field,
+                                          request=request)
+
+        config['content_css'] = config['portal_url'] + '/base.css'
+        del config['customplugins']
+        del config['plugins']
+        del config['theme']
+
+        config['content_css'] = '++resource++plone.app.widgets-tinymce-content.css'
+        args['pattern_options'] = {
+            'relatedItems': {
+                'vocabularyUrl': config['portal_url'] +
+                '/@@getVocabulary?name=plone.app.vocabularies.Catalog'
+            },
+            'upload': {
+                'initialFolder': initial,
+                'currentPath': current_path,
+                'baseUrl': config['document_base_url'],
+                'relativePath': '@@fileUpload',
+                'uploadMultiple': False,
+                'maxFiles': 1,
+                'showTitle': False
+            },
+            'tiny': config,
+            # This is for loading the languages on tinymce
+            'loadingBaseUrl': '++plone++static/components/tinymce-builded/js/tinymce',
+            'prependToUrl': 'resolveuid/',
+            'linkAttribute': 'UID',
+            'prependToScalePart': '/@@images/image',
+            'folderTypes': utility.containsobjects.replace('\n', ','),
+            'imageTypes': utility.imageobjects.replace('\n', ','),
+            'anchorSelector': utility.anchor_selector,
+            'linkableTypes': utility.linkable.replace('\n', ',')
+        }
+    else:
+        # Plone 5
+        # They are setted on the body
+        pattern_options = getMultiAdapter(
+            (context, request, field),
+            name="tinymce_settings")()['data-pat-tinymce']
+        args['pattern_options'] = json.loads(pattern_options)
+    return args
+
+from plone.app.widgets import utils
+
+utils.get_tinymce_options = get_tinymce_options
